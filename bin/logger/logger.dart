b296843c7bb345ed1da1e651e275f2c77742c1ba
rostikKalabishka <rostik31052002@gmail.com>
2025-01-11 08:55:28 +0200

    change

--- bin/logger/logger.dart
+++ bin/logger/logger.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   Timer.periodic(Duration(seconds: 30), (timer) async {
     try {
       final currentUser =
